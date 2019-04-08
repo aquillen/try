a604587a122bd285522101c99cba4a168077eb32
serenaflint <44509543+serenaflint@users.noreply.github.com>
2019-04-08 15:13:15 -0400

    added is_stuck(obj)

--- particle.py
+++ particle.py
@@ -7,6 +7,7 @@ Spyder Editor
 class particle:
     
     #constructor
+    #free should be a boolean; true = free, false = stuck
     def __init__(self,mass,x,y,z,vx,vy,vg,v0,ax,ay,ag,free):
         self.mass = mass;
         self.x = x;
@@ -22,8 +23,15 @@ class particle:
         self.free = free;
         
     #update position?
+    #needs to also take in account other forces
     def update_pos(a, v, dt):
         dv = a*dt;
         dx = v* dt;
         return dv, dx
+    
+    #gets value of free from an existing object
+    #returns boolean
+    def is_stuck(obj):
+        stuck = getattr(obj,'free')
+        return stuck
 
