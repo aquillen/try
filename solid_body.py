04c6cc3526bde2a0f81862ac1f7f8d28e37f7ced
Alice Quillen <aquillen@users.noreply.github.com>
2019-04-09 07:24:21 -0400

    Update solid_body.py

--- solid_body.py
+++ solid_body.py
@@ -1,5 +1,23 @@
 import numpy as np
 
+class stuck_particle():
+  constructor
+  def __init__(self,mass,radius,x,y,z):
+    self.x = x
+    self.y = y
+    self.z = z
+    self.mass = mass
+    self.radius = radius
 
 class solid_body():
-  __init
+  #constructor, construct with a single central stuck particle
+  def __init__(self,mass,radius):
+    self.N=1  # how many particles in the solid body
+    self.stuck_particles = []
+    first_particle = stuck_particle(mass,radius,0,0,0)
+    self.stuck_particles = np.append(self.stuck_particles,first_particle)
+    
+  
+  
+  
+
