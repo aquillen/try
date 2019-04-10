57ff516e1f745cefb2db93ed22d6049d3b338891
serenaflint <44509543+serenaflint@users.noreply.github.com>
2019-04-10 14:12:07 -0400

    fixing errors

--- particle.py
+++ particle.py
@@ -7,7 +7,7 @@ Spyder Editor
 class free_particle:
     
     #constructor
-    def __init__(self,mass,radius,x,y,z,vx,vy,vz,ax,ay,az):
+    def __init__(self,mass,radius,x,y,z,vx,vy,vg,ax,ay,ag):
         self.mass = mass;
         self.radius = radius;
         self.x = x;
@@ -22,7 +22,7 @@ class free_particle:
      
     #update position?
     #needs to also take in account other forces
-    def update_pos(dt):
+    def update_pos(self, dt):
         self.x = self.vx*dt;
         self.y = self.vy*dt;
         self.z = self.vz*dt;
